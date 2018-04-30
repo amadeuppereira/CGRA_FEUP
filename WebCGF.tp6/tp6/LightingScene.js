c323c7fd907238ee65f5b80451936874981b7849
Amadeu Pereira <amadeu.dx1@gmail.com>
2018-04-30 18:43:16 +0100

    added interface for lights

--- WebCGF.tp6/tp6/LightingScene.js
+++ WebCGF.tp6/tp6/LightingScene.js
@@ -156,6 +156,26 @@ class LightingScene extends CGFscene
 	{
 		for (var i = 0; i < this.lights.length; i++)
 			this.lights[i].update();
+
+		if(this.light0)
+			this.lights[0].enable();
+		else
+			this.lights[0].disable();
+
+		if(this.light1)
+			this.lights[1].enable();
+		else
+			this.lights[1].disable();
+
+		if(this.light2)
+			this.lights[2].enable();
+		else
+			this.lights[2].disable();
+			
+		if(this.light3)
+			this.lights[3].enable();
+		else
+			this.lights[3].disable();
 	}
 
 

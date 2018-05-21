32a8014106cd941fc2f88356f796c814a229ad19
Nuno Lopes <up201605337@fe.up.pt>
2018-05-21 15:51:44 +0100

    crane done

--- WebCGF.tp6/tp6/MyCrane.js
+++ WebCGF.tp6/tp6/MyCrane.js
@@ -14,6 +14,7 @@ class MyCrane extends CGFobject
 		this.yRotation = 0;
 		this.xRotation = 0;
 		this.direction = "none";
+		this.direction2 = "none";
 
 		this.cylinder = new MyCylinder(this.scene,8,20);
 		this.side = new MyCircle(scene, 12);
@@ -23,64 +24,53 @@ class MyCrane extends CGFobject
 	drawCraneComponents(){
 		//Base Cylinder
 		this.scene.pushMatrix();
-		this.scene.rotate(this.yRotation,0,1,0);
-		this.scene.pushMatrix();
 		this.scene.scale(2,1,2);
 		this.scene.rotate(-90 * degToRad,1,0,0);
 		this.completeCylinder();
 		this.scene.popMatrix();
-		this.scene.popMatrix();
 
 		//Biggest Cylinder
 		this.scene.pushMatrix();
-		this.scene.rotate(this.yRotation,0,1,0);
 		this.scene.rotate(30 * degToRad,1,0,0);
-		this.scene.pushMatrix();
 		this.scene.translate(0,1,-0.5);
 		this.scene.scale(0.6,10,0.6);
 		this.scene.rotate(- 90 * degToRad,1,0,0);
 		this.completeCylinder();
 		this.scene.popMatrix();
-		this.scene.popMatrix();
 
 		//Turning Cylinder
 		this.scene.pushMatrix();
-		this.scene.rotate(this.yRotation,0,1,0);
-		this.scene.pushMatrix();
-		this.scene.translate(-0.5,10.4,5.4);
-		this.scene.scale(1,0.8,0.8);
+		this.scene.translate(-0.6,10.4,5.4);
+		this.scene.scale(1.2,1,1);
 		this.scene.rotate(-90 * degToRad,0,0,1);
 		this.scene.rotate(-90 * degToRad,1,0,0);
 		this.scene.rotate(this.xRotation,0,0,1);
 		this.completeCylinder();
 		this.scene.popMatrix();
-		this.scene.popMatrix();
 
 		//Hand Cylinder
 		this.scene.pushMatrix();
-		this.scene.rotate(this.yRotation,0,1,0);
-		this.scene.pushMatrix();
 		this.scene.translate(0,10.4,6.1);
-		this.scene.pushMatrix();
 		this.scene.rotate(this.xRotation,1,0,0);
 		this.scene.scale(0.6,0.6,6);
 		this.completeCylinder();
 		this.scene.popMatrix();
-		this.scene.popMatrix();
-		this.scene.popMatrix();
 
 		//Cable
 		this.scene.pushMatrix();
-		this.scene.rotate(this.yRotation,0,1,0);
-		this.scene.pushMatrix();
-		this.scene.translate(0,5-Math.sin(this.xRotation)*6,5+Math.cos(this.xRotation)*7);
-		this.scene.scale(0.1,5,0.1);
+		this.scene.translate(0,4.5-Math.sin(this.xRotation)*6,5+Math.cos(this.xRotation)*7);
+		this.scene.scale(0.1,6,0.1);
 		this.scene.rotate(-90 * degToRad,1,0,0);
 		this.completeCylinder();
 		this.scene.popMatrix();
-		this.scene.popMatrix();
 
 		//Magnet
+		this.scene.pushMatrix();
+		this.scene.translate(0,4.5-Math.sin(this.xRotation)*6,5+Math.cos(this.xRotation)*7);
+		this.scene.scale(1,0.5,1);
+		this.scene.rotate(-90 * degToRad,1,0,0);
+		this.completeCylinder();
+		this.scene.popMatrix();
 	}
 
 	completeCylinder(){
@@ -101,6 +91,7 @@ class MyCrane extends CGFobject
 	
 	display(){
 		this.scene.pushMatrix();
+		this.scene.rotate(this.yRotation,0,1,0);
 		this.drawCraneComponents();
 		this.scene.popMatrix();
 	}
@@ -114,7 +105,12 @@ class MyCrane extends CGFobject
 				this.xRotation += 0.01;
 		if(this.direction == "down")
 			if(this.xRotation >= -0.2)
-			this.xRotation -= 0.01;
+				this.xRotation -= 0.01;
+
+		if(this.direction2 == "right")
+			this.yRotation += 0.01;
+		if(this.direction2 == "left")
+			this.yRotation -= 0.01;
 
 	};
 }
